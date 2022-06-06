3fe412fe744b0c85c054275a48e653e772eb1cd0
alami <alami@km.ru>
2022-06-06 17:19:23 +0500

    01-07 compare Singleton

--- src/main/java/ru/alisher/springcourse/TestSprint.java
+++ src/main/java/ru/alisher/springcourse/TestSprint.java
@@ -13,6 +13,13 @@ public class TestSprint {
         //TestBean testBean = context.getBean("testBean", TestBean.class);
         //Music music = context.getBean("musicBean", Music.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class); //new MusicPlayer(music);
+        MusicPlayer musicPlayerSecond = context.getBean("musicPlayer",MusicPlayer.class);
+
+        boolean comparision = musicPlayer == musicPlayerSecond;
+        System.out.println(comparision);
+        System.out.println(musicPlayer);
+        System.out.println(musicPlayerSecond);
+
         musicPlayer.playMusic();
         System.out.println(musicPlayer.getName());
         System.out.println(String.valueOf(musicPlayer.getVolume()));
